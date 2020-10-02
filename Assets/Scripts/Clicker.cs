3064b20efcca98a51f34639a2d60037860e6255a
Vorimo <ilyaivanov654@gmail.com>
2020-10-02 18:22:53 +0300

    Main functionality

---
+++ Assets/Scripts/Clicker.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Clicker : MonoBehaviour {
+
+    public int score;
+
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start() {
+        score = 0;
+    }
+
+    public void Click() {
+        score++;
+        scoreText.text = score.ToString();
+    }
+}
